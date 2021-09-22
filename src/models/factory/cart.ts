40cf3e931a63a7aa367d0459f6265ca55c1c6ae1
Lucas Caniggia <lucascaniggia5@gmail.com>
2021-09-22 15:15:39 -0300

    Add Cart model for MongoDB and Firebase

--- src/models/factory/cart.ts
+++ src/models/factory/cart.ts
@@ -2,6 +2,8 @@ import { IntItem } from '/common/interfaces';
 import { ModelType } from '/common/enums';
 import { CartModel } from '/models/fs/cart';
 import { CartModelMySQL } from '/models/mysql/cart';
+import { CartModelMongoDB } from '/models/mongodb/cart';
+import { CartModelFirebase } from '/models/firebase/cart';
 
 interface IntModel {
   get: (id?: string) => Promise<IntItem | IntItem[]>
@@ -18,6 +20,12 @@ export class CartModelFactory {
         return new CartModelMySQL('mysql');
       case ModelType.sqlite:
         return new CartModelMySQL('sqlite');
+      case ModelType.localMongo:
+        return new CartModelMongoDB('local');
+      case ModelType.mongoAtlas:
+        return new CartModelMongoDB('atlas');
+      case ModelType.firebase:
+        return new CartModelFirebase();
       default:
         return new CartModel();
     }
--- src/models/mongodb/cart.ts
+++ src/models/mongodb/cart.ts
@@ -1,13 +1,13 @@
 import Config from '/config';
 import { IntItem, BaseIntItem } from '/common/interfaces';
+import { productsModel } from '/models/mongodb/product';
 import moment from 'moment';
 import mongoose from 'mongoose';
 import { NotFound } from '/errors';
-import { productsMock } from '/mocks/products';
 
 const ProductSchema = new mongoose.Schema<BaseIntItem>({
   name: { type: String, require: true, max: 100 },
-  description: { type: String, require: true, max: 100 },
+  description: { type: String, require: true, max: 500 },
   code: { type: String, require: true, max: 14 },
   price: { type: Number, require: true, max: 5000 },
   photo: { type: String, require: true },
@@ -23,12 +23,12 @@ ProductSchema.set('toJSON', {
   }
 });
 
-export const productsModel = mongoose.model<BaseIntItem>('products', ProductSchema);
-
-export class ProductsModelMongoDB {
+export class CartModelMongoDB {
   private dbURL: string;
+  private cart;
   private products;
   constructor(type: 'local' | 'atlas') {
+    this.cart = mongoose.model<BaseIntItem>('cart', ProductSchema);
     this.products = productsModel;
     if (type === 'local') {
       this.dbURL = 'mongodb://0.0.0.0:27017/ecommerce';
@@ -36,17 +36,7 @@ export class ProductsModelMongoDB {
       this.dbURL = `mongodb+srv://${Config.MONGO_ATLAS_USER}:${Config.MONGO_ATLAS_PASSWORD}@${Config.MONGO_ATLAS_CLUSTER}/${Config.MONGO_ATLAS_DB}?retryWrites=true&w=majority`;
     }
     mongoose.connect(this.dbURL)
-      .then(() => {
-        console.log('Mongo DB connected!');
-        this.get()
-          .then((products) => {
-            if (products.length === 0) {
-              this.products.insertMany(productsMock)
-                .then(() => console.log('Products added successfully'))
-                .catch((e) => console.log(e));
-            }
-          })
-          .catch((e) => console.log(e));
+      .then(() => { console.log('Mongo DB connected!');
       })
       .catch((e) => console.log(e));
   }
@@ -55,10 +45,11 @@ export class ProductsModelMongoDB {
     try {
       let output: IntItem | IntItem[] = [];
       if (id) {
-        const document = await this.products.find({ _id: id });
+        const document = await this.cart.findById(id);
         if (document) output = ((document as unknown) as IntItem);
+        else throw new NotFound('Product does not exist on cart.');
       } else {
-        const products = await this.products.find();
+        const products = await this.cart.find();
         output = (products as unknown) as IntItem[];
       }
       return output;
@@ -71,28 +62,36 @@ export class ProductsModelMongoDB {
     }
   }
 
-  async save(data: IntItem): Promise<IntItem> {
-    const newProduct = await new this.products(data);
-    await newProduct.save();
-    return (newProduct as unknown) as IntItem;
-  }
-
-  async update(id: string, data: IntItem): Promise<IntItem> {
+  async save(id: string): Promise<IntItem> {
     try {
-      const updatedProduct = await this.products.findByIdAndUpdate(id, data, { new: true, runValidators: true, rawResult: true });
-      return (updatedProduct.value as unknown) as IntItem;
-    } catch (e) {
-      if (e instanceof mongoose.Error.CastError) {
-        throw new NotFound('Product to update does not exist.');
+      const addedProduct = await this.products.findById(id);
+      if (addedProduct) {
+        const productJSON = addedProduct.toJSON();
+        productJSON._id = productJSON.id;
+        delete productJSON.id;
+
+        const newProduct = await new this.cart(productJSON as IntItem);
+        await newProduct.save();
+        return newProduct as IntItem;
       } else {
-        throw { error: e, message: 'Product could not be updated.' };
+        throw new NotFound('Product to add does not exist on cart.');
+      }
+    } catch (e) {
+      if (e instanceof NotFound) {
+        throw e;
+      } else if (e instanceof mongoose.Error.CastError) {
+        throw new NotFound('Product to add does not exist on cart.');
+      } else {
+        throw { error: e, message: 'An error occurred when loading products.' };
       }
     }
   }
 
-  async delete(id: string): Promise<void> {
+  async delete(id: string): Promise<IntItem[]> {
     try {
-      await this.products.findByIdAndRemove(id);
+      await this.cart.findByIdAndRemove(id);
+      const cartProducts = await this.get();
+      return cartProducts as IntItem[];
     } catch (e) {
       if (e instanceof mongoose.Error.CastError) {
         throw new NotFound('Product to delete does not exist.');
